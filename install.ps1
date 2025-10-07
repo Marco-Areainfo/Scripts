566d37d320966e99d26a1c4cbe20685d8845d589
Marco-Areainfo <marco.sinini@areainfo.it>
2025-10-07 16:50:37 +0200

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,1 @@
+winget install --id=Google.Chrome -e  ; winget install --id=VideoLAN.VLC -e  ; winget install --id=PDFgear.PDFgear -e  ; winget install --id=M2Team.NanaZip -e  ; winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  ; winget install --id=dotPDN.PaintDotNet -e 
