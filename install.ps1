7c5241e450ffc2739312ee86a36dd4fa775195fd
Marco-Areainfo <marco.sinini@areainfo.it>
2025-10-13 11:58:53 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,10 @@
-winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  ; winget install --id=Google.Chrome -e  ; winget install --id=VideoLAN.VLC -e  ; winget install --id=PDFgear.PDFgear -e  ; winget install --id=M2Team.NanaZip -e  ; winget install --id=dotPDN.PaintDotNet -e 
+winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  ; 
+winget install --id=Google.Chrome -e  ; 
+winget install --id=VideoLAN.VLC -e  ; 
+winget install --id=PDFgear.PDFgear -e  ; 
+winget install --id=M2Team.NanaZip -e  ; 
+winget install --id=dotPDN.PaintDotNet -e 
+
 Write-Host "Fatto."
 Read-Host -Prompt "Premi Invio per uscire"
 exit
