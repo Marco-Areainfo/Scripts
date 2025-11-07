53a2a247cdfee8d3bcd6ff0ec10bd0ac2218f93e
Marco-Areainfo <marco.sinini@areainfo.it>
2025-11-07 17:37:43 +0100

    Update basic.ps1

--- basic.ps1
+++ basic.ps1
@@ -2,9 +2,10 @@ winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  ;
 winget install --id=Google.Chrome -e  ; 
 winget install --id=VideoLAN.VLC -e  ; 
 winget install --id=PDFgear.PDFgear -e  ; 
-winget install --id=M2Team.NanaZip -e  ; 
+winget install --id=M2Team.NanaZip -e --scope machine  ; 
 winget install --id=dotPDN.PaintDotNet -e ;
-winget install --id=CodecGuide.K-LiteCodecPack.Full -e
+winget install --id=CodecGuide.K-LiteCodecPack.Full -e ;
+winget install --id Microsoft.PowerToys -e --scope machine --override "/quiet PowertoysAutostart=false"
 
 Write-Host "Fatto."
 Read-Host -Prompt "Premi Invio per uscire"
