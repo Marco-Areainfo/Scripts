70d7f161c6983c7c38b37f4bd162077e0eaa6093
Marco-Areainfo <marco.sinini@areainfo.it>
2025-11-25 10:01:11 +0100

    2

---
+++ test3.ps1
@@ -0,0 +1,18 @@
+# Add-TrustedSites-HKCU.ps1
+# Adds Trusted Sites entries for the current user (HKCU)
+
+$TrustedSites = @(
+  'server',
+  '192.168.1.3'
+)
+
+$basePath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains'
+
+foreach ($site in $TrustedSites) {
+    $keyPath = Join-Path -Path $basePath -ChildPath $site
+    if (-not (Test-Path $keyPath)) {
+        New-Item -Path $keyPath -Force | Out-Null
+    }
+    # Create the '*' DWORD = 2 (Trusted Sites)
+    New-ItemProperty -Path $keyPath -Name '*' -PropertyType DWord -Value 2 -Force | Out-Null
+}
