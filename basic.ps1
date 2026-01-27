95ee24605c89adf32125ce5f851c7b909f02d516
Marco-Areainfo <marco.sinini@areainfo.it>
2026-01-27 12:17:11 +0100

    Update basic.ps1

--- basic.ps1
+++ basic.ps1
@@ -1,11 +1,11 @@
-winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  ; 
-winget install --id=Google.Chrome -e  ; 
-winget install --id=VideoLAN.VLC -e  ; 
-winget install --id=PDFgear.PDFgear -e  ; 
-winget install --id=M2Team.NanaZip -e  ; 
-winget install --id=dotPDN.PaintDotNet -e ;
-winget install --id=CodecGuide.K-LiteCodecPack.Full -e ;
-winget install --id=Microsoft.PowerToys -e --scope machine 
+winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e -s winget ; 
+winget install --id=Google.Chrome -e -s winget ; 
+winget install --id=VideoLAN.VLC -e -s winget ; 
+winget install --id=PDFgear.PDFgear -e -s winget ; 
+winget install --id=M2Team.NanaZip -e -s winget ; 
+winget install --id=dotPDN.PaintDotNet -e -s winget ;
+winget install --id=CodecGuide.K-LiteCodecPack.Full -e -s winget ;
+winget install --id=Microsoft.PowerToys -e -s winget --scope machine 
 
 Write-Host "Fatto."
 Read-Host -Prompt "Premi Invio per uscire"
