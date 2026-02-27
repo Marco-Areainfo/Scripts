8ba58af1d3cf4df7cfc82b79193a5f34e5ee468a
Marco-Areainfo <marco.sinini@areainfo.it>
2026-02-27 18:36:12 +0100

    Update java.ps1

--- java.ps1
+++ java.ps1
@@ -1,8 +1,14 @@
 winget install --id eclipseadoptium.temurin.8.jre ; 
 winget install --id karakun.OpenWebStart 
 
-Write-Host "Prima di laciare gli applet Java aprire OpenWebStart Settings > JVM Manager > Find local"
-Write-Host "Dopo andare in Settings e cambiare Update strategy con Do not download any version"
-Write-Host "Eseguire gli applet Java come Administrator"
+Write-Host "Prima di laciare gli applet Java aprire " -nonewline
+Write-Host "OpenWebStart Settings > JVM Manager > Find local" -foregroundcolor red
+Write-Host "Dopo andare in " -nonewline
+Write-Host "Settings " -foregroundcolor red -nonewline
+Write-Host "e cambiare " -nonewline
+Write-Host "Update strategy " -foregroundcolor red -nonewline
+Write-Host "con " -nonewline
+Write-Host "Do not download any version" -foregroundcolor red
+Write-Host "Eseguire gli applet Java come Administrator al primo avvio"
 Read-Host -Prompt "Premi Invio per uscire"
 exit
