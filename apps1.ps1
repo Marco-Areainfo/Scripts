26c8a724074240c1396d26b078460b2fb6d6ae02
Marco-Areainfo <marco.sinini@areainfo.it>
2026-03-16 18:35:35 +0100

    k

--- apps1.ps1
+++ apps1.ps1
@@ -8,17 +8,21 @@ Write-Host -foregroundcolor red @'
 ************************        ********  ***     *******    ********  **  **     **  **    *******    
 '@
 
+$LogPath = "C:\Logs\Install_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
+Start-Transcript -Path $LogPath | Out-Null
+
+winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e -s winget ; 
+winget install --id=Google.Chrome -e -s winget ; 
+winget install --id=VideoLAN.VLC -e -s winget ; 
+winget install --id=PDFgear.PDFgear -e -s winget ; 
+winget install --id=M2Team.NanaZip -e -s winget ; 
+winget install --id=dotPDN.PaintDotNet -e -s winget ;
+winget install --id=CodecGuide.K-LiteCodecPack.Full -e -s winget ;
+winget install --id=Microsoft.PowerToys -e -s winget --scope machine 
+
 Write-Host ""
-Write-Host "Cancellare dalle reti memorizzate quelle con SSID contenti questi simboli (>,<,/,\,|,:,*,?)"
-Write-Host "o l'esportazione si interrompera'."
-Read-Host -Prompt "Premi Invio per iniziare"
-New-Item -Path "C:\" -Name "WIFI" -ItemType "Directory"
-netsh wlan export profile interface=wi-fi key=clear folder=C:\WIFI
-Write-Host ""
-Write-Host "Completato, le reti sono state esportate in C:\WIFI, le password sono visibili."
-Write-Host "Per importarle in un altro PC copiare la cartella WIFI in C:\ ed eseguire il seguente comando in CMD:"
-Write-Host @'
-for  %a  in  (C:\WIFI\*.xml)  do  netsh  wlan  add  profile  "%a"
-'@ -foregroundcolor green
+Write-Host "Terminato. Log disponible in C:\Logs."
+Write-Host "Per ritentare le installazioni o aggiornare le app eseguire nuovamente lo script."
+Stop-Transcript | Out-Null
 Read-Host -Prompt "Premi Invio per uscire"
 exit
--- test.ps1
+++ test.ps1
@@ -8,10 +8,7 @@ Write-Host -foregroundcolor red @'
 ************************        ********  ***     *******    ********  **  **     **  **    *******    
 '@
 
-$LogPath = "C:\Logs\ScriptLog_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
-Start-Transcript -Path $LogPath | Out-Null
 Write-Host ""
 Write-Host "TEST"
 Read-Host -Prompt "Premi Invio per uscire"
-Stop-Transcript | Out-Null
 exit
