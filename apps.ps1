65e54ea5323b5990ad46bc935a85fcdd055a5877
Marco-Areainfo <marco.sinini@areainfo.it>
2026-03-24 18:18:05 +0100

    apps

--- apps.ps1
+++ apps.ps1
@@ -11,6 +11,9 @@ Write-Host -foregroundcolor red @'
 $LogPath = "C:\Logs\Install_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
 Start-Transcript -Path $LogPath | Out-Null
 
+$options = [System.Management.Automation.Host.ChoiceDescription[]] @("&No", "&Yes")
+$choice = $host.ui.PromptForChoice("", "Installare anche LibreOffice?", $options, 0)
+
 winget install --id Microsoft.DotNet.Framework.DeveloperPack_4 -e -s winget ; 
 winget install --id Google.Chrome -e -s winget ; 
 winget install --id VideoLAN.VLC -e -s winget ; 
@@ -20,30 +23,10 @@ winget install --id dotPDN.PaintDotNet -e -s winget ;
 winget install --id CodecGuide.K-LiteCodecPack.Full -e -s winget ;
 winget install --id Microsoft.PowerToys -e -s winget --scope machine 
 
-Function Read-YesNoChoice {
-	Param (
-        [Parameter(Mandatory=$true)][String]$Title,
-		[Parameter(Mandatory=$true)][String]$Message,
-		[Parameter(Mandatory=$false)][Int]$DefaultOption = 0
-    )
-	
-	$No = New-Object System.Management.Automation.Host.ChoiceDescription '&No', 'No'
-	$Yes = New-Object System.Management.Automation.Host.ChoiceDescription '&Yes', 'Yes'
-	$Options = [System.Management.Automation.Host.ChoiceDescription[]]($No, $Yes)
-	
-	return $host.ui.PromptForChoice($Title, $Message, $Options, $DefaultOption)
-}
-$choice = Read-YesNoChoice -Title "Installare anche LibreOffice?" -Message "Yes or No?" -DefaultOption 0
-switch($choice)
-{
-	# No
-	0 {
-		Write-Host "LibreOffice non verra' installato."
-	}
-	# Yes
-	1 {
-		winget install --id TheDocumentFoundation.LibreOffice -e -s winget --scope machine
-	}
+if ($choice -eq 1) {
+    winget install --id TheDocumentFoundation.LibreOffice -e -s winget --scope machine
+} else {
+    Write-Host "LibreOffice non verra' installato." -ForegroundColor Yellow
 }
 
 Write-Host ""
