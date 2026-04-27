08bde33bd40f69e3ed37db2febe36b62f1aed8cc
Marco-Areainfo <marco.sinini@areainfo.it>
2026-04-27 09:19:15 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -9,21 +9,6 @@ Write-Host -foregroundcolor red @'
 '@
 
 
-$url = "https://raw.githubusercontent.com/Marco-Areainfo/Scripts/refs/heads/main/test.ps1"
-
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    
-    Write-Host "==============================" -ForegroundColor Yellow
-    Write-Host " NECESSARI PRIVILEGI DI ADMIN" -ForegroundColor Yellow
-    Write-Host "==============================" -ForegroundColor Yellow
-    Write-Host ""
-    Read-Host "Premere Invio per richiedere privilegi elevati"
-    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iex (irm '$url')`"" -Verb RunAs
-    exit
-}
-
-Write-Host "In esecuzione come Administrator" -ForegroundColor Green
 
 Write-Host ""
 Write-Host "TEST"
