189f4c881a81d06e5c3c696af0dcba1f432f98eb
Marco-Areainfo <marco.sinini@areainfo.it>
2026-09-10 18:30:02 +0200

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/Marco-Areainfo/Scripts/refs/heads/main/appsnw.ps1"
+$url = "https://raw.githubusercontent.com/Marco-Areainfo/Scripts/refs/heads/main/apps.ps1"
 
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
